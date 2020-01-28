0a9b73ac9ef22a1a422711b728bbdea9597074ee
mdshazif <60009041+mdshazif@users.noreply.github.com>
2020-01-28 17:49:42 +0530

    Update 5_MethodSignature.cs

--- 5_MethodSignature.cs
+++ 5_MethodSignature.cs
@@ -1,7 +1,7 @@
 	var(averageSalary, numberOfEmployee) = await SomeCalculation(0L, 10, 0L == 10L);
   
   //Method signature looks like below code - C#-7. Couldn't run since I don't have latest C# version installed.
-  public async (long averageSalary, long numberOfEmployee) SomeCalculation(long salary, long numEmployee, bool isValueEqual)
+  public (long averageSalary, long numberOfEmployee) SomeCalculation(long salary, long numEmployee, bool isValueEqual)
         {
             var avgSalary = (salary == 0L) ? salary : salary / numEmployee;
 	     //...
